769fc184212686c6410a03732edba4fccf335edf
ac2393921 <ac2393921@gmail.com>
2023-04-26 01:35:07 +0900

    fix: moduleのimport先を変更

--- cmd/server.go
+++ cmd/server.go
@@ -18,7 +18,7 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graphql.NewExecutableSchema(graphql.Config{Resolvers: &graphql.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
